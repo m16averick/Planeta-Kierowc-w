0b9893030b86bdc2e91cc602f3939aeed80e7a48
m16averick <37076920+m16averick@users.noreply.github.com>
2020-05-16 21:26:44 +0200

    Update Modeli

--- Model fizyczny.sql
+++ Model fizyczny.sql
@@ -1,79 +1,71 @@
-drop table Zlecenia;
-drop table Dostepnosc;
-drop table Users;
-drop table Role;
+drop table if exists Protokoly;
+drop table if exists Zlecenia;
+drop table if exists Przedzial_czasu;
+drop table if exists Users;
+drop table if exists Role;
 
 
-drop table Protokoly;
-
-
-
-
-
-
-
-CREATE TABLE If not exists Role (
-    ID INT,
-    Nazwa varchar(50),
-    PRIMARY KEY (ID)
+CREATE TABLE Role(
+    ID_Rola INT NOT NULL AUTO_INCREMENT,
+    Nazwa varchar(50) NOT NULL,
+    PRIMARY KEY (ID_Rola)
 );
 
-CREATE TABLE If not exists Users (
-    ID INT,
+INSERT INTO Role (Nazwa) VALUES ('Koordynator');
+INSERT INTO Role (Nazwa) VALUES ('Kierowca');
+INSERT INTO Role (Nazwa) VALUES ('Klient');
+
+
+CREATE TABLE Users (
+    ID_User INT NOT NULL AUTO_INCREMENT,
     Imie_nazwisko varchar(50),
     Numer_tel INT,
     Rola_ID INT,
-    PRIMARY KEY (ID),
-    FOREIGN KEY (Rola_ID) REFERENCES Role(ID)
+    PRIMARY KEY (ID_User),
+    FOREIGN KEY (Rola_ID) REFERENCES Role(ID_Rola)
 );
 
-CREATE TABLE If not exists Dostepnosc (
-    ID INT,
-    Kierowca_ID INT,
-    OD DATETIME,
-    DO DATETIME,
-    PRIMARY KEY (ID),
-    FOREIGN KEY (Kierowca_ID) REFERENCES Users(ID)
+INSERT INTO Users (Imie_nazwisko, Numer_tel, Rola_ID) VALUES ('Piotr Gruszynski', '500300400', 1);
+INSERT INTO Users (Imie_nazwisko, Numer_tel, Rola_ID) VALUES ('Tomasz Maciwoda', '500300400', 2);
+INSERT INTO Users (Imie_nazwisko, Numer_tel, Rola_ID) VALUES ('Jan Kowalski', '500300400', 3);
+
+
+CREATE TABLE Przedzial_czasu (
+    ID_Przedzial INT unsigned NOT NULL AUTO_INCREMENT,
+    Kierowca_ID INT NOT NULL,
+    OD DATETIME NOT NULL,
+    DO DATETIME NOT NULL,
+    PRIMARY KEY (ID_Przedzial),
+    FOREIGN KEY (Kierowca_ID) REFERENCES Users(ID_User)
 );
 
+INSERT INTO Przedzial_czasu (Kierowca_ID, OD, DO) VALUES (1, '2020-05-06 20:00', '2020-05-07 20:00');
 
 
-CREATE TABLE If not exists Protokoly (
-    ID INT,
-    Plik varchar(100),
-    PRIMARY KEY (ID)
-);
-
-
-CREATE TABLE If not exists Zlecenia (
-    ID INT,
+CREATE TABLE Zlecenia (
+    ID_Zlecenie INT NOT NULL AUTO_INCREMENT,
     Miejsce_odbioru varchar(50),
     Czas_odbioru DATETIME,
     Miejsce_zdania varchar(50),
     Czas_zdania DATETIME,
     Kierowca_ID INT,
     Koordynator_ID INT,
-    Klient_ID INT,
-    Protokol_ID INT,
-    PRIMARY KEY (ID),
-    FOREIGN KEY (Kierowca_ID) REFERENCES Users(ID),
-    FOREIGN KEY (Koordynator_ID) REFERENCES Users(ID),
-    FOREIGN KEY (Klient_ID) REFERENCES Users(ID),
-    FOREIGN KEY (Protokol_ID) REFERENCES Protokoly(ID)
+    PRIMARY KEY (ID_Zlecenie),
+    FOREIGN KEY (Kierowca_ID) REFERENCES Users(ID_User),
+    FOREIGN KEY (Koordynator_ID) REFERENCES Users(ID_User)
 );
 
-INSERT INTO Role VALUES (1, 'Koordynator');
-INSERT INTO Role VALUES (2, 'Kierowca');
-INSERT INTO Role VALUES (3, 'Klient');
-
-INSERT INTO Users VALUES (1, 'Piotr Gruszynski', '500300400', 1);
-INSERT INTO Users VALUES (2, 'Tomasz Maciwoda', '500300400', 2);
-INSERT INTO Users VALUES (3, 'Jan Kowalski', '500300400', 3);
-INSERT INTO Dostepnosc VALUES (1, 1, '2020-05-06 20:00', '2020-05-07 20:00');
+INSERT INTO Zlecenia (Miejsce_odbioru, Czas_odbioru, Miejsce_zdania, Czas_zdania, Kierowca_ID, Koordynator_ID) VALUES ('Kamienna 13 Wroclaw', '2020-05-06 20:00', 'Powstancow 30 Warszawa', '2020-05-07 20:00', 1, 2);
 
 
-INSERT INTO Protokoly VALUES (1, 'sciezkadopliku');
-INSERT INTO Zlecenia VALUES (1, 'Kamienna 13 Wroclaw', '2020-05-06 20:00', 'Powstancow 30 Warszawa', '2020-05-07 20:00', 1, 2, 3, 1);
+CREATE TABLE Protokoly (
+    ID_Protokol INT unsigned NOT NULL AUTO_INCREMENT,
+    Zlecenie_ID INT NOT NULL,
+    Plik varchar(100),
+    PRIMARY KEY (ID_Protokol),
+    FOREIGN KEY (Zlecenie_ID) REFERENCES Zlecenia(ID_Zlecenie)
+);
 
+INSERT INTO Protokoly (Zlecenie_ID, Plik) VALUES (1, 'sciezkadopliku');
 
-select ID, Miejsce_odbioru, Czas_odbioru, Miejsce_zdania, Czas_zdania, Kierowca, Koordynator, Klient, Protokol from Zlecenia natural left join (select ID as Kierowca_ID, Imie_nazwisko as Kierowca from Users) as foo2 natural left join (select ID as Koordynator_ID, Imie_nazwisko as Koordynator from Users) as foo3 natural left join (select ID as Klient_ID, Imie_nazwisko as Klient from Users) as foo4 natural left join (select ID as Protokol_ID, Plik as Protokol from Protokoly) as foo5 ;
+select ID_Zlecenie, Miejsce_odbioru, Czas_odbioru, Miejsce_zdania, Czas_zdania, Kierowca, Koordynator, Protokol from Zlecenia natural left join (select ID_User as Kierowca_ID, Imie_nazwisko as Kierowca from Users) as foo2 natural left join (select ID_User as Koordynator_ID, Imie_nazwisko as Koordynator from Users) as foo3 natural left join (select ID_Protokol as Protokol_ID, Plik as Protokol from Protokoly) as foo4 ;
